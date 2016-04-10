44a8e90dc7809887dac142181bdaae14e21b4865
quandragoon <quannguyenca15@gmail.com>
2016-04-10 16:18:14 -0700

    Added echo entrypoint

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ Then start the image 'cache':
     $ docker run -d -p 6080:8080 cache
 
 Then hit http://192.168.99.101:6080
+
+## Test new changes
+First, package it:
+  
+    $ mvn clean package
+
+Then, copy the .war file to container. Run '$ docker ps' to see the container's name.
+
+    $ docker cp target/cache-java-ws-1.0-SNAPSHOT.war <container>:/var/lib/jetty/webapps/ROOT.war
+
+Restart the container:
+
+    $ docker restart <container>
+
+
--- src/main/java/cache/ws/RestApi.java
+++ src/main/java/cache/ws/RestApi.java
@@ -1,8 +1,6 @@
 package cache.ws;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -19,5 +17,12 @@ public class RestApi {
     @Produces(MediaType.TEXT_PLAIN)
     public Response ping () throws IOException {
         return Response.status(200).entity("pong").build();
+    }
+
+    @GET
+    @Path("/echo")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response echo (@QueryParam("name") String name) {
+        return Response.status(200).entity("Hello " + name).build();
     }
 }
