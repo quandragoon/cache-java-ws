b1a22dae05615dd7161072dbe83eab74ad8315cb
quandragoon <quannguyenca15@gmail.com>
2016-04-10 22:21:52 -0700

    Added basic POST and GET APIs

--- src/main/java/cache/ws/RestApi.java
+++ src/main/java/cache/ws/RestApi.java
@@ -7,6 +7,9 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by qdnguyen on 4/9/16.
@@ -16,6 +19,33 @@ import java.io.IOException;
 public class RestApi {
 
     Logger logger = LoggerFactory.getLogger(RestApi.class);
+
+    private static HashMap<String, List<String> > itemsMap = new HashMap<>();
+
+    @GET
+    @Path("/list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getList (@DefaultValue("") @QueryParam("uid") String uid) {
+        if (!"".equals(uid) && itemsMap.containsKey(uid)) {
+            return Response.status(200).entity(itemsMap.get(uid)).build();
+        }
+        return Response.status(200).entity("INVALID ARGS").build();
+    }
+
+    @POST
+    @Path("/append")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response addToList (@DefaultValue("") @QueryParam("uid") String uid,
+                               @DefaultValue("" )@QueryParam("iid") String iid) {
+        if (!"".equals(uid) && !"".equals(iid)) {
+            if (!itemsMap.containsKey(uid)) {
+                itemsMap.put(uid, new ArrayList<>());
+            }
+            itemsMap.get(uid).add(iid);
+            return Response.status(200).entity("SUCCESS").build();
+        }
+        return Response.status(200).entity("INVALID ARGS").build();
+    }
 
     @GET
     @Path("/ping")
